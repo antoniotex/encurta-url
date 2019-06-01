4b64e4fb2a286c51adcff87509497732858a5657
Antonio Carlos <antoniotx.dev@gmail.com>
2019-06-01 13:32:00 -0300

    Teste de redirecionamento

--- config/server.js
+++ config/server.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
