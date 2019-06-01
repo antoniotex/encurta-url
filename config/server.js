b2446e63cfe8d83bf4d8e305194852cf0d6175a6
Antonio Carlos <antoniotx.dev@gmail.com>
2019-06-01 01:01:36 -0300

    primeiro deploy

--- config/server.js
+++ config/server.js
@@ -20,4 +20,13 @@ mongoose.connect(bd, { useNewUrlParser: true })
 
 app.use('/api/encurtador', encurtador)
 
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) =>{
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 module.exports = app
