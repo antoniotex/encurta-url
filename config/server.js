d39e9819e106f993ff6a394fc69d7a3bbc09c4dc
Antonio Carlos <antoniotx.dev@gmail.com>
2019-06-01 14:16:55 -0300

    altera projeto

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const path = require('path')
+// const path = require('path')
 
 var app = express()
 var encurtador = require('../routes/api/encurtador')
@@ -39,13 +39,13 @@ mongoose.connect(bd, { useNewUrlParser: true })
 
 app.use('/api/encurtador', encurtador)
 
-if(process.env.NODE_ENV === 'production'){
-  // Set static folder
-  app.use(express.static('client/build'))
+// if(process.env.NODE_ENV === 'production'){
+//   // Set static folder
+//   app.use(express.static('client/build'))
 
-  app.get('*', (req, res) =>{
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+//   app.get('*', (req, res) =>{
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   })
+// }
 
 module.exports = app
