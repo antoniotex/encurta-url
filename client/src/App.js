0e681b95db58c7e133f59d7761b399bb9f6a27e6
Antonio Carlos <antoniotx.dev@gmail.com>
2019-06-01 00:55:53 -0300

    Estilo parcial front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'reactstrap'
+import Header from './components/header/header';
+import axios from 'axios'
+import { Alert } from 'reactstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      novoPost: {},
+      novosDados: {}
+    }
+  }
+
+  handleSearch = (e) => {
+    const value = e.target.value
+    this.setState({
+      novoPost: {
+        urlOriginal: value,
+        apelidoCustom: ''
+      }
+    })
+  }
+
+  criaNovaUrl = () => {
+    axios.post('/api/encurtador', this.state.novoPost).then(res => {
+      this.setState({
+        novosDados: {
+          urlOriginal: res.data.urlOriginal,
+          urlEncurtada: res.data.urlEncurtada
+        }
+      }, console.log(this.state))
+    })
+  }
+
+
+  render(){
+    const dados = this.state.novosDados
+    return (
+      <div className="App">
+        <Header/>
+        <section className="form">
+          <h1>Encurtador de URL</h1>
+          <input type="text" name="urlOriginal" onInput={this.handleSearch} placeholder="Digite ou cole sua URL aqui..."/>
+          <Button onClick={this.criaNovaUrl} color="success">Encurtar!</Button>
+          {dados.urlEncurtada && <Alert color="success">
+            { `Parabéns, aqui está sua URL encurtada: ${dados.urlEncurtada}` }
+          </Alert>}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
