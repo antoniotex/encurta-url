a4cfb6e17afb2ba07b947650131cd5419ebeca93
Antonio Carlos <antoniotx.dev@gmail.com>
2019-06-02 18:55:59 -0300

    add verificação de url original

--- routes/api/encurtador.js
+++ routes/api/encurtador.js
@@ -21,19 +21,25 @@ router.get('/get/:apelido', function(req, res){
 })
 
 router.post('/', function(req, res){
-
   const apelido = crypto.randomBytes(3).toString('hex');
   const urlEncurtada = `enc-it.firebaseapp.com/${req.body.apelido ? req.body.apelido : apelido}`
   const urlOriginal = req.body.urlOriginal
 
-  const novoItem = new Encurtador({
-    urlOriginal: urlOriginal.includes('http') ? urlOriginal : `https://${urlOriginal}`,
-    apelido: req.body.apelido ? req.body.apelido : apelido,
-    urlEncurtada: urlEncurtada
-  })
-  novoItem.save().then(function(item){
-    res.json(item)
-  })
+  Encurtador.findOne({urlOriginal: urlOriginal}, (error, item) => {
+    if(item !== null){
+      res.json(item)
+    }else{
+      const novoItem = new Encurtador({
+        urlOriginal: urlOriginal.includes('http') ? urlOriginal : `https://${urlOriginal}`,
+        apelido: req.body.apelido ? req.body.apelido : apelido,
+        urlEncurtada: urlEncurtada
+      })
+      novoItem.save().then(function(item){
+        res.json(item)
+      })
+    }
+  }).catch(erro => console.log('erro vindo: ', erro))
+
 })
 
 module.exports = router
